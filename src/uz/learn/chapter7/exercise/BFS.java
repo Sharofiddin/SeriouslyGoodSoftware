752c4c67d567534dac3776578128bca4ef58f009
sharofiddin <pardayev.sharofiddin@gmail.com>
2025-08-20 10:59:31 +0500

    readable

--- src/uz/learn/chapter7/exercise/BFS.java
+++ src/uz/learn/chapter7/exercise/BFS.java
@@ -3,46 +3,47 @@ package uz.learn.chapter7.exercise;
 import java.util.Arrays;
 import java.util.Stack;
 
+enum Status {
+  READY,
+	WAITING,
+	PROCESSED
+}
 /**
 	 * The BFS implemented in code to use
 	 *
 	 * @param a Structure to perform the search on a graph, adjacency matrix etc.
-	 * @param vertices The vertices to use
-	 * @param source The Source
+	 * @param verticeCount The verticeCount to use
+	 * @param sourceVertice The Source
 	 */
 public class BFS {
-  public static void bfsImplement(byte[][] a, int vertices, int source) { // passing adjacency matrix and number of vertices
-  	byte [] b =  new byte[vertices]; //flag container containing status
-																		 //of each vertices
+  public static void bfsImplement(byte[][] adjacent, int verticeCount, int sourceVertice) { // passing adjacency matrix and number of verticeCount
+  	Status [] statuses =  new Status[verticeCount]; //flag container containing status
+																		 //of each verticeCount
 																		 
-		Arrays.fill(b,(byte)-1); //status initialization
-											/*
-											   code   status
-											   -1  = ready
-											    0  = waiting
-											    1  = processed */
-		Stack<Integer> st = new Stack<>(); //operational stack
-		st.push(source);  //assigning source
-		while(!st.isEmpty()){
-      b[st.peek()] = (byte)0; //assigning waiting status
-      System.out.println(st.peek());
-		  int pop = st.peek();
-		  b[pop] = (byte)1; //assigning processed status
-		  st.pop(); //removing head of the queue
-		  for(int i = 0; i <vertices;i++){
-        if(a[pop][i]!= 0 && b[i] != (byte)0 && b[i] != (byte)1 ){
-          st.push(i);
-					b[i] = (byte)0; //assigning waiting status
+		Arrays.fill(statuses,Status.READY); //status initialization
+		Stack<Integer> processingStack = new Stack<>(); //operational stack
+		processingStack.push(sourceVertice);  //assigning sourceVertice
+		while(!processingStack.isEmpty()){
+      statuses[processingStack.peek()] = Status.WAITING; 
+      System.out.println(processingStack.peek());
+		  int currentVertice = processingStack.peek();
+		  statuses[currentVertice] = Status.PROCESSED; 
+		  processingStack.pop(); //moving to next vertice
+		  for(int i = 0; i <verticeCount;i++){
+        if(isVerticeReadyAndConnected(adjacent[currentVertice][i], statuses[i])){
+          processingStack.push(i);
+					statuses[i] = Status.WAITING; 
 				}
 			}
 		}
-
-											
   }
+	private static boolean isVerticeReadyAndConnected(int connectionStatus, Status processingStatus){
+     return connectionStatus != 0 && processingStatus == Status.READY;
+	}
 	public static void main(String[] args) {
-	  byte a[][] = {{0,1,1},{1,0,0},{1,0,0}};	
-		int vertices = 3;
-		int source = 2;
-		bfsImplement(a, vertices, source);
+	  byte adjacent[][] = {{0,1,1},{1,0,0},{1,0,0}};	
+		int verticeCount = 3;
+		int sourceVertice = 2;
+		bfsImplement(adjacent, verticeCount, sourceVertice);
 	}
 }
